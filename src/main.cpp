80281382cde9459e21ede025f1e34db155feb44d
Timothy Hutchins <tgh6407@gmail.com>
2024-06-29 20:06:15 -0500

    Using steamworks_dumper as a library works
    Reformated code in main.cpp and incorperated a basic dumper from steamworks_dumper

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,23 @@
 
 #include "ManifestManager.hpp"
 
+#include "steamworks_dumper.h"
+
 int main(int argc, char **argv) {
     argparse::ArgumentParser arguments("OSWUpdater");
 
-    // Handle arguments
-    arguments.add_argument("--headers")
-        .help("Dumped header directory");
-    arguments.add_argument("--download-dir")
+    /* Handle Arguments */
+    arguments.add_argument("--old-headers") // For comparison
+        .help("Old dumped header directory");
+    arguments.add_argument("--download") // The directory to download files to
         .help("Directory to download and extract files to")
         .default_value("./download");
-    arguments.add_argument("--dump-dir")
+    arguments.add_argument("--dump") // The directory to dump files to
         .help("Directory to dump files to")
-        .default_value("./extracted");
+        .default_value("./dumped");
+    arguments.add_argument("--header-format") // The format to write header files
+        .help("The file extention to write for header files")
+        .default_value("h");
 
     try {
         arguments.parse_args(argc, argv);
@@ -25,12 +30,26 @@ int main(int argc, char **argv) {
         std::cerr << arguments;
         return 1;
     }
-    
-    ManifestManager manifestManager(ManifestManager::MANIFEST_URL.data(), std::filesystem::path(arguments.get<std::string>("--download-dir")), "client_manifest.vdf");
-    DownloadManager ubuntu_12Download("https://client-update.akamai.steamstatic.com/" + manifestManager.Get("bins_ubuntu12").file, "./download", manifestManager.Get("bins_ubuntu12").file);
-    ubuntu_12Download.DownloadFile();
 
-    ExtractionManager ubuntu_12Extraction(ubuntu_12Download.GetDownloadDirectory().string() + '/' + ubuntu_12Download.GetDownloadFileName(), std::filesystem::path("./extracted/"), manifestManager.Get("bins_ubuntu12").name);
-    ubuntu_12Extraction.Extract();
+    std::string downloadDirectory = arguments.get<std::string>("--download");
+    std::string dumpDirectory = arguments.get<std::string>("--dump");
     
+    ManifestManager manifestManager(ManifestManager::MANIFEST_URL.data(), std::filesystem::path(downloadDirectory), "client_manifest.vdf"); // Download and manage Client Manifest VDF
+
+    std::string ubuntu12DownloadURL = "https://client-update.akamai.steamstatic.com/" + manifestManager.Get("bins_ubuntu12").file;
+    DownloadManager ubuntu_12Download(ubuntu12DownloadURL, downloadDirectory, manifestManager.Get("bins_ubuntu12").file);
+    ubuntu_12Download.DownloadFile(); // Download client libs
+    
+    std::string ubuntu_12ArchiveLocation = ubuntu_12Download.GetDownloadDirectory().string() + '/' + ubuntu_12Download.GetDownloadFileName();
+    ExtractionManager ubuntu_12Extraction(ubuntu_12ArchiveLocation, std::filesystem::path(downloadDirectory + "/extracted/"), manifestManager.Get("bins_ubuntu12").name);
+    ubuntu_12Extraction.Extract(); // Extract client libs
+
+    /* Dumps from steamclient.so using steamworks_dumper as a library */
+    std::string steamclientlib = ubuntu_12Extraction.GetFullOutputPath().string() + '/' + "ubuntu12_32/steamclient.so";
+
+    if (!std::filesystem::exists(dumpDirectory)) {
+        std::filesystem::create_directories(dumpDirectory);
+    }
+
+    Dump(steamclientlib, dumpDirectory, true); // Used to be main() of steamworks_dumper
 }
